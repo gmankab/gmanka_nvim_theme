08ae2b6598843f3e15496c8751b4a914e2f2f4f1
gmanka <gmankab@gmail.com>
2025-06-03 10:54:03 +0500

    [f] name

--- colors/gmanka.lua
+++ colors/gmanka.lua
@@ -14,8 +14,8 @@ vim.g.colors_name = 'gmanka.nvim'
 --
 -- The performance impact of this call can be measured in the hundreds of
 -- *nanoseconds* and such could be considered "production safe".
-package.loaded['lush_theme.gnome.nvim'] = nil
+package.loaded['lush_theme.gmanka.nvim'] = nil
 
 -- include our theme file and pass it to lush to apply
-require('lush')(require('lush_theme.gnome.nvim'))
+require('lush')(require('lush_theme.gmanka.nvim'))
 
